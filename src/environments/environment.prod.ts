6e8399bee316ba5e42752c7218fbade69737023f
tjabitta <tjabitta@gmail.com>
2020-05-25 13:01:21 +0100

    updates!

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/',
+  apiUrl: 'http://161.35.102.235:8080/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/',
+  apiUrl: 'http://161.35.102.235:8080/',
 };
