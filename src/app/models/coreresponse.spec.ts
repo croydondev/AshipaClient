5e95ce9ac4b7de29c5a711a2efde95f335b54774
tjabitta <tjabitta@gmail.com>
2020-06-21 18:49:38 +0100

    clean up!

--- src/app/models/coreresponse.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Coreresponse } from './coreresponse';
-
-describe('Coreresponse', () => {
-  it('should create an instance', () => {
-    expect(new Coreresponse()).toBeTruthy();
-  });
-});
